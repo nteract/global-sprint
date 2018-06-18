8509fd4cdfb37122e61680e3097105f1d94a1f41
Kyle Kelley <rgbkrk@gmail.com>
2018-06-18 10:25:47 -0700

    start up a next.js site for the sprint

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Host from "../host.md";
+import Lead from "../lead.md";
+import Participate from "../participate.md";
+
+export default () => (
+  <div>
+    <h1>nteract global sprint</h1>
+    <Participate />
+    <Lead />
+    <Host />
+  </div>
+);
